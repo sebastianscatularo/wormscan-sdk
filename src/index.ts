dc07704d2e9080dc2a4bbb9d0b951ffd1999a9f4
Valentino Conti <valeen.conti@hotmail.com>
2023-01-27 14:07:58 -0300

    health check example endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const testFn = (a: number, b: number) => a + b;
+import { IHealth } from "./interfaces";
+
+class WormScanClient {
+  constructor() {}
+
+  async checkHealth() {
+    return fetch("http://api.staging.wormscan.io/api/v1/health").then(response => {
+      if (response.ok) {
+        return response.json() as Promise<IHealth>;
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+}
+
+export default WormScanClient;
