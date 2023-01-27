630366a403f4ae8ed6cd2373f707c8b9d0d2fe88
Ricardo Olarte <raop155@hotmail.com>
2023-01-27 13:39:21 -0500

    Add axios

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import { IHealth } from "./interfaces";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
-class WormScanClient {
+class WormScanSDK {
+  private API_BASE_URL: string = "http://api.staging.wormscan.io/api/v1";
+
   constructor() {}
 
-  async checkHealth() {
-    return fetch("http://api.staging.wormscan.io/api/v1/health").then(response => {
-      if (response.ok) {
-        return response.json() as Promise<IHealth>;
-      } else {
-        throw new Error(response.statusText);
+  protected async request<T>(endpoint: string, options?: AxiosRequestConfig) {
+    const url = `${this.API_BASE_URL}${endpoint}`;
+    const config = {
+      ...options,
+    };
+
+    try {
+      const response = await axios<T>(url, config);
+      const { data } = response;
+      return data;
+    } catch (e: any) {
+      const errors = e as Error | AxiosError;
+      if (!axios.isAxiosError(errors)) {
+        // do whatever you want with native error
+        throw new Error(errors.message);
       }
-    });
+      // do what you want with your axios error
+      throw new Error(errors.message);
+    }
+  }
+
+  public async checkHealth() {
+    return this.request<IHealth>("/health");
   }
 }
 
-export default WormScanClient;
+export default WormScanSDK;
