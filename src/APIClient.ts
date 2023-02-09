52f22275569598cbf01c925ba10c739e66a6ba90
Juan Sebastian Scatularo <sebastianscatularo@gmail.com>
2023-02-09 10:20:54 -0300

    add typing and test for Governor abstraction

--- src/APIClient.ts
+++ src/APIClient.ts
@@ -12,11 +12,10 @@ export class AxiosClient implements APIClient {
     this._client = axios.create({ baseURL });
   }
 
-  async doGet<T>(path: string, params?: any) {
+  async doGet<T>(path: string, params?: any): Promise<T> {
     try {
       const response = await this._client.get(path, { params });
-      const data = response?.data;
-      return data as T;
+      return response.data;
     } catch (e: any) {
       const errors = e as Error | AxiosError;
       throw new Error(errors.message);
--- src/Governor.ts
+++ src/Governor.ts
@@ -1,47 +1,279 @@
 import { APIClient } from "./APIClient";
-import { DefaultPageRequest, PageRequest } from "./PageRequest";
+import { ChainId } from "./ChainId";
+import { _get } from "./Objects";
+
+interface Notional {
+  id: string;
+  chainId: ChainId;
+  nodeName: string;
+  notionalLimit: number;
+  availableNotional: number;
+  maxTransactionSize: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Node<C = any, T = any> {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  nodeName: string;
+  counter: number;
+  chains: C[];
+  tokens: T[];
+}
+
+export interface ChainNotionalAvailable
+  extends Omit<Notional, "maxTransactionSize" | "notionalLimit"> {}
+export interface NotionalAvailable
+  extends Omit<
+    Notional,
+    "id" | "maxTransactionSize" | "notionalLimit" | "nodeName" | "createdAt" | "updatedAt"
+  > {}
+export interface ChainNotionalLimit extends Omit<Notional, "availableNotional"> {}
+export interface NotionalLimit
+  extends Omit<Notional, "id" | "nodeName" | "createdAt" | "updatedAt"> {}
+
+export interface VAA {
+  sequence: number;
+  chainId: ChainId;
+  emitterAddress: string;
+  notionalValue: number;
+  txHash: string;
+  releaseTime: number;
+}
+
+export interface Chain {
+  chainId: ChainId;
+  enqueuedVAA: Omit<VAA, "releaseTime">[];
+}
+
+export interface NodeConfiguration extends Node<ChainConfiguration, TokenConfiguration> {}
+
+export interface ChainConfiguration {
+  chainId: ChainId;
+  notionalLimit: number;
+  bigTransactionSize: number;
+}
+
+export interface TokenConfiguration {
+  originChainId: ChainId;
+  originAddress: string;
+  price: number;
+}
+
+export interface NodeStatus extends Omit<Node<ChainStatus>, "tokens" | "counter"> {}
+export interface ChainStatus {
+  chainId: ChainId;
+  remainingAvailableNotional: number;
+  emitters: EmitterStatus[];
+}
+
+export interface EmitterStatus {
+  emitterAddress: string;
+  totalEnqueuedVAAs: number;
+}
 
 export class Governor {
   constructor(private readonly _client: APIClient) {}
 
-  async getConfiguration<T>(guardianId: number = null, page: PageRequest = DefaultPageRequest) {
+  async getConfiguration(): Promise<NodeConfiguration[]>;
+  async getConfiguration(guardianId: string): Promise<NodeConfiguration>;
+  async getConfiguration(guardianId: string = null) {
     const effectivePath = guardianId ? `/governor/config/${guardianId}` : "/governor/config";
-    return this._client.doGet<T>(effectivePath, {
-      params: { ...page },
-    });
+    const payload = await this._client.doGet<any>(effectivePath);
+    const result = _get(payload, "data", []).map(this._mapNodeConfiguration);
+    return guardianId ? result.pop() : result;
   }
 
-  async getEnqueuedVaas<T>(chainId: number = null, page: PageRequest = DefaultPageRequest) {
+  async getEnqueuedVaas(): Promise<Chain[]>;
+  async getEnqueuedVaas(chainId: ChainId): Promise<VAA[]>;
+  async getEnqueuedVaas(chainId: ChainId = null) {
     const effectivePath = chainId ? `/governor/enqueued_vaa/${chainId}` : "/governor/enqueued_vaa";
-    return this._client.doGet<T>(effectivePath, { params: { ...page } });
+    const payload = await this._client.doGet<any>(effectivePath);
+    const result = _get(payload, "data", []);
+    return result.map(chainId ? this._mapVAA : this._mapChainVAA);
   }
 
-  async getLimit(page: PageRequest = DefaultPageRequest) {
-    return this._client.doGet("/governor/limit", { params: { ...page } });
+  //TODO API is returning 500
+  async getMaxAvailableNotional(chainId: ChainId) {
+    return this._client.doGet(`/governor/notional/max_available/${chainId}`);
   }
 
-  async getMaxAvailableNotional(chainId: number, page: PageRequest = DefaultPageRequest) {
-    return this._client.doGet(`/governor/notional/max_available/${chainId}`, {
-      params: { ...page },
-    });
-  }
-
-  async getAvailableNotional(chainId: number = null, page: PageRequest = DefaultPageRequest) {
+  async getAvailableNotional(): Promise<NotionalAvailable[]>;
+  async getAvailableNotional(chainId: ChainId): Promise<ChainNotionalAvailable[]>;
+  async getAvailableNotional(chainId: ChainId = null) {
     const effectivePath = chainId
       ? `/governor/notional/available/${chainId}`
       : "/governor/notional/available";
-    return this._client.doGet(effectivePath, { params: { ...page } });
+    const payload = await this._client.doGet<any>(effectivePath);
+    const result = _get(payload, "data", []);
+    return result.map(chainId ? this._mapChainAvailableNotional : this._mapAvailableNotional);
   }
 
-  async getNotionalLimit(chainId: number = null, page: PageRequest = DefaultPageRequest) {
+  async getNotionalLimit(): Promise<NotionalLimit[]>;
+  async getNotionalLimit(chainId: ChainId): Promise<ChainNotionalLimit[]>;
+  async getNotionalLimit(chainId: ChainId = null) {
     const effectivePath = chainId
       ? `/governor/notional/limit/${chainId}`
       : "/governor/notional/limit";
-    return this._client.doGet(effectivePath, { params: { ...page } });
+    const payload = await this._client.doGet<any>(effectivePath);
+    const result = _get(payload, "data", []);
+    return result.map(chainId ? this._mapChainNotionalLimit : this._mapNotionalLimit);
   }
 
-  async getStatus(guardianId: number = null, page: PageRequest = DefaultPageRequest) {
+  async getStatus(): Promise<NodeStatus[]>;
+  async getStatus(guardianId: string): Promise<NodeStatus>;
+  async getStatus(guardianId: string = null) {
     const effectivePath = guardianId ? `/governor/status/${guardianId}` : "/governor/status";
-    return this._client.doGet(effectivePath, { params: { ...page } });
+    const payload = await this._client.doGet<any>(effectivePath);
+    if (guardianId) {
+      const result = _get(payload, "data", {});
+      return this._mapStatus(result);
+    } else {
+      const result = _get(payload, "data", []);
+      return result.map(this._mapStatus);
+    }
   }
+
+  async getLimit(): Promise<NotionalLimit[]> {
+    const payload = await this._client.doGet<{ data: any }>("/governor/limit");
+    const result = _get(payload, "data", []);
+    return result.map(this._mapNotionalLimit);
+  }
+
+  private _mapNodeConfiguration = ({
+    id,
+    nodename,
+    createdAt,
+    updatedAt,
+    counter,
+    chains,
+    tokens,
+  }: any) => ({
+    id,
+    nodeName: nodename,
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(updatedAt),
+    counter,
+    chains: chains?.map(this._mapChainConfiguration),
+    tokens: tokens?.map(this._mapTokenConfiguration),
+  });
+
+  private _mapStatus = ({ id, nodename, chains, createdAt, updatedAt }: any): NodeStatus => ({
+    id,
+    nodeName: nodename,
+    chains: chains.map(this._mapChainStatus),
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(updatedAt),
+  });
+
+  private _mapChainStatus = ({
+    chainid,
+    remainingavailablenotional,
+    emitters,
+  }: any): ChainStatus => ({
+    chainId: ChainId[chainid] as unknown as ChainId,
+    remainingAvailableNotional: remainingavailablenotional,
+    emitters: emitters.map(this._mapEmitterStatus),
+  });
+
+  private _mapEmitterStatus = ({ emitteraddress, totalenqueuedvaas }: any): EmitterStatus => ({
+    emitterAddress: emitteraddress,
+    totalEnqueuedVAAs: totalenqueuedvaas,
+  });
+
+  private _mapChainConfiguration = ({
+    chainid,
+    notionallimit: notionalLimit,
+    bigtransactionsize: bigTransactionSize,
+  }: any): ChainConfiguration => ({
+    chainId: ChainId[chainid] as unknown as ChainId,
+    notionalLimit,
+    bigTransactionSize,
+  });
+
+  private _mapTokenConfiguration = ({
+    originchainid,
+    originaddress: originAddress,
+    price,
+  }: any): TokenConfiguration => ({
+    originChainId: ChainId[originchainid] as unknown as ChainId,
+    originAddress,
+    price,
+  });
+
+  private _mapChainNotionalLimit = ({
+    id,
+    chainId,
+    nodename,
+    notionalLimit,
+    maxTransactionSize,
+    createdAt,
+    updatedAt,
+  }: any): ChainNotionalLimit => ({
+    id,
+    chainId: ChainId[chainId] as unknown as ChainId,
+    nodeName: nodename,
+    notionalLimit,
+    maxTransactionSize,
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(updatedAt),
+  });
+
+  private _mapNotionalLimit = ({
+    chainid,
+    chainId,
+    availableNotional,
+    notionalLimit,
+    maxTransactionSize,
+  }: any): NotionalLimit => ({
+    chainId: ChainId[chainid | chainId] as unknown as ChainId,
+    availableNotional,
+    notionalLimit,
+    maxTransactionSize,
+  });
+
+  private _mapChainAvailableNotional = ({
+    id,
+    chainId,
+    nodeName,
+    availableNotional,
+    createdAt,
+    updatedAt,
+  }: any): ChainNotionalAvailable => ({
+    id,
+    nodeName,
+    chainId: ChainId[chainId] as unknown as ChainId,
+    availableNotional,
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(updatedAt),
+  });
+
+  private _mapAvailableNotional = ({ chainId, availableNotional }: any): NotionalAvailable => ({
+    chainId: ChainId[chainId] as unknown as ChainId,
+    availableNotional,
+  });
+
+  private _mapChainVAA = ({ chainId, enqueuedVaas }: any): Chain => ({
+    chainId: ChainId[chainId] as unknown as ChainId,
+    enqueuedVAA: enqueuedVaas.map(this._mapVAA),
+  });
+
+  private _mapVAA = ({
+    chainid,
+    chainId,
+    sequence,
+    emitterAddress,
+    notionalValue,
+    txHash,
+    releaseTime,
+  }: any): VAA => ({
+    chainId: ChainId[chainid || chainId] as unknown as ChainId,
+    sequence,
+    emitterAddress,
+    notionalValue,
+    txHash,
+    releaseTime,
+  });
 }
